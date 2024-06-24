0319748bd9b550a4d345053fe3f0baf6532e5769
USER <USER@1.209.180.56>
2024-06-24 10:57:36 +0900

    java

--- java_00/src/java_00/HelloJava.java
+++ java_00/src/java_00/HelloJava.java
@@ -1,10 +1,17 @@
 package java_00;
-
+/**
+ * 작성자 : 권지선
+ * 작성일자 : 2024.06.18
+ * 코멘트 : 자바 예제 소스 코드
+ */
 public class HelloJava {
 		
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Java!!");
+		
+		String test1 = "안녕하세요";
+		String _test = "";
 		
 	}
 }
---
+++ java_00/src/java_00/VariableEx.java
@@ -0,0 +1,24 @@
+package java_00;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		int varNum = 5;
+		//String varString = "5";
+		
+		int varNumber;
+		varNumber = 100;
+		
+		System.out.println("숫자 변수 1 [" + varNum + "] 입니다.");
+		System.out.println("숫자 변수 2 ["+ varNumber + "] 입니다.");
+		System.out.println("숫자 변수 3 ["+ varNumber + "] 입니다.");
+		
+		System.out.println("숫자 1과 숫자2 를 더하면 ? ["+ (varNum + varNumber) +"]");
+		*/
+		
+		
+	}
+
+}
